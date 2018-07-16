7a87a47c496077edddbb209448000cf93cd6fb5e
beranemo <beranemo@gmail.com>
2018-07-17 03:46:12 +0800

    authenticate_admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,16 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+  
   def index
+  end
+  
+  private
+  
+  def authenticate_admin
+    unless current_user.role == "admin"
+      flash[:alert] = "你沒有相關權限，不允許此操作"
+      redirect_to root_path
+    end
   end
 end
