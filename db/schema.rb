6f3ecb77a9bb7ac4d2b5093fdebf1f768e12638c
beranemo <beranemo@gmail.com>
2018-07-22 14:38:23 +0800

    generate model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180722062329) do
+ActiveRecord::Schema.define(version: 20180722063703) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
