99c1ce7fd67d2b464b5eef3ab4df54a91384ab37
beranemo <beranemo@gmail.com>
2018-07-22 14:27:18 +0800

    add admin to users

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'ffaker'
 gem 'kaminari'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
+gem 'annotate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  tel           :string
+#  address       :string
+#  opening_hours :string
+#  description   :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  image         :string
+#
+
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   mount_uploader :image, PhotoUploader
