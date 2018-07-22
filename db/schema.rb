e78d1bd2733d423b3107bb469954dc8c0458d476
beranemo <beranemo@gmail.com>
2018-07-22 15:24:32 +0800

    generate seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180722063703) do
+ActiveRecord::Schema.define(version: 20180722064016) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180722063703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,25 @@
 User.create!(
   email: "test@test.com",
   password: "123456",
-  role: "admin"
+  role: "admin",
+  admin: true
 )
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "中式料理" },
+  { name: "日本料理" },
+  { name: "義大利料理" },
+  { name: "墨西哥料理" },
+  { name: "素食料理" },
+  { name: "美式料理" },
+  { name: "複合式料理" }
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end
+puts "Category created!"
